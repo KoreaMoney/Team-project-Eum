d2dbb45e69728a7e2cebc6f74b99c71f5e7906f4
hodu_Mom <aldud7798555@gmail.com>
2023-03-07 03:42:25 +0900

    구매중 모달 버그수정

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -376,7 +376,8 @@ console.log( 'post: ' ,post);
                         onClick={onClickToggleModal}
                         aria-label="구매자명단"
                       >
-                        구매자 ({myOnSale?.length === 0 ? '0' : myOnSale?.length})
+                        구매자 (
+                        {myOnSale?.length === 0 ? '0' : myOnSale?.length})
                       </a.LikeSubmitButton>
                     ) : (
                       <a.LikeSubmitButton
@@ -398,10 +399,10 @@ console.log( 'post: ' ,post);
 
                     {saveUser?.uid === post?.[0].sellerUid ? (
                       <a.LikeSubmitButton
-                        onClick={onClickApplyBuy}
-                        aria-label="판매중"
+                        onClick={onClickToggleModal}
+                        aria-label="구매자명단"
                       >
-                        판매중({myOnSale?.length ? myOnSale?.length : 0})
+                        구매자({myOnSale?.length ? myOnSale?.length : 0})
                       </a.LikeSubmitButton>
                     ) : (
                       <a.LikeSubmitButton
